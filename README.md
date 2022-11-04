7763c6e43d4c2acbf5649d22fc4c6a138394083c
jzinferno <jekazinferno@gmail.com>
2022-11-04 19:00:23 +0200

    jd2019: Initial TWRP devive tree

---
+++ README.md
@@ -0,0 +1,14 @@
+# TWRP device tree for Lenovo Z5s (L78071)
+
+## Device specifications
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Octa-core (2x 2.2 GHz Kryo 360, 6x 1.7 GHz)
+CHIPSET | Qualcomm Snapdragon 710 (SDM710)
+GPU     | Adreno 616, 500 MHz
+Memory  | 4/6 GB (LPDDR4X)
+Shipped Android Version | Pie
+Storage | 64/128 GB Micron S0J9K8
+Battery | 3300 mAh Li-Ion
+Display | LTPS-LCD 1080x2340 16M 409ppi
+Extras  | microSD Card
---
+++ twrp_jd2019.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from jd2019 device
+$(call inherit-product, device/lenovo/jd2019/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := jd2019
+PRODUCT_NAME := twrp_jd2019
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo L78071
+PRODUCT_MANUFACTURER := lenovo
